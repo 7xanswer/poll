109b84ac2b4abf648a767dab01636babbe368f1b
Guillaume Le Louarn <7guillaumelelouarn@gmail.com>
2021-04-08 09:40:08 +0200

    init template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    public function index(): Response
+    {   
+        $page_name = 'Dashboard';
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'page_name' => $page_name,
+            //object.property
+        ]);
+    }
+}
